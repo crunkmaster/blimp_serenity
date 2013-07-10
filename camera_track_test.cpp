73b51f66046fbe80e14e9b742f0ed743cab38b6f
Ian McGaunn <ianmcgaunn@Ians-MacBook-Air.local>
2013-07-10 11:03:09 -0400

    started tracking two markers

--- camera_track_test.cpp
+++ camera_track_test.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 Mat GetThresholdedImage( Mat imgHSV, Scalar lower, Scalar upper ) ;
 Point computeCentroid( Mat &mask ) ;
+Mat combineImages( Mat img1, Mat img2 ) ;
 
 int main( int argc, char *argv[] ) {
 
@@ -20,23 +21,21 @@ int main( int argc, char *argv[] ) {
 
   Mat scaled ;
   Point blue_centroid ;
-  Point black_centroid ;
-
-
-  Scalar upper = Scalar(50, 250, 200) ;
+  Point red_centroid ;
 
   /* these two sets of Scalars should be updated to the 
      markers that are placed on the blimp */
 
-  Scalar blue_lower = Scalar(115, 150, 60) ;
-  Scalar blue_upper = Scalar(125, 220, 150) ;
+  Scalar blue_lower = Scalar(115, 120, 60) ;
+  Scalar blue_upper = Scalar(125, 220, 254) ;
 
-  Scalar black_lower = Scalar(0, 0, 0) ;
-  Scalar black_upper = Scalar(0, 0, 0) ;
+  Scalar red_lower = Scalar(0, 150, 60) ;
+  Scalar red_upper = Scalar(15, 240, 254) ;
 
   namedWindow( "Source", CV_WINDOW_AUTOSIZE ) ;
   namedWindow( "Blue Thresholded", CV_WINDOW_AUTOSIZE ) ;
-  namedWindow( "Black Thresholded", CV_WINDOW_AUTOSIZE ) ;
+  namedWindow( "Red Thresholded", CV_WINDOW_AUTOSIZE ) ;
+  namedWindow( "Thresholded", CV_WINDOW_AUTOSIZE ) ;
   while ( true ) {
 
     /* grab a new frame from the camera */
@@ -46,27 +45,30 @@ int main( int argc, char *argv[] ) {
 
     Mat imgHSV = Mat(scaled.size(), 8, 1) ;
     Mat blue_thresholded  = Mat(scaled.size(), 8, 1) ;
-    Mat black_thresholded = Mat(scaled.size(), 8, 1) ;
+    Mat red_thresholded = Mat(scaled.size(), 8, 1) ;
+    Mat both_thresholded = Mat(scaled.size(), 8, 1) ;
 
     /* convert image to HSV and blur it */
     cvtColor( scaled, imgHSV, CV_BGR2HSV );
-    blur( imgHSV, imgHSV, Size( 5, 5 ) ) ;
+    blur( imgHSV, imgHSV, Size( 10, 10 ) ) ;
 
     /* create some binary images */
     blue_thresholded = GetThresholdedImage( imgHSV, blue_lower, blue_upper ) ;
-    black_thresholded = GetThresholdedImage( imgHSV, black_lower, black_upper ) ;
+    red_thresholded = GetThresholdedImage( imgHSV, red_lower, red_upper ) ;
+    both_thresholded = combineImages( red_thresholded, blue_thresholded ) ;
     blue_centroid = computeCentroid( blue_thresholded ) ;
-    black_centroid = computeCentroid( black_thresholded ) ;
+    red_centroid = computeCentroid( red_thresholded ) ;
 
     cout << blue_centroid.x << " " << blue_centroid.y << endl ;
 
     /* draw a circle around the center point */
-    circle( scaled, blue_centroid, 5, upper, -1 ) ;
-    circle( scaled, black_centroid, 5, upper, -1 ) ;
+    circle( scaled, blue_centroid, 5, blue_upper, -1 ) ;
+    circle( scaled, red_centroid, 5, red_upper, -1 ) ;
 
     imshow( "Source", scaled );
     imshow( "Blue Thresholded", blue_thresholded ) ;
-    imshow( "Black Thresholded", black_thresholded ) ;
+    imshow( "Red Thresholded", red_thresholded ) ;
+    imshow( "Thresholded", both_thresholded ) ;
 
     if ( waitKey(30) >= 0 ) break ;
 
@@ -88,4 +90,15 @@ Point computeCentroid( Mat &mask ) {
   Moments m = moments(mask, true) ;
   Point center( m.m10/m.m00, m.m01/m.m00 ) ;
   return center ;
+}
+
+Mat combineImages( Mat img1, Mat img2 ) {
+  int rows = img1.rows ;
+  int cols = img1.cols ;
+  Mat dst = cvCreateMat(rows, 2 * cols, img1.type()) ;
+  Mat tmp = dst(cv::Rect(0, 0, cols, rows));
+  img1.copyTo(tmp); 
+  tmp = dst(cv::Rect(cols, 0, cols, rows)) ;
+  img2.copyTo(tmp);
+  return dst ;
 }
